54d23dd848da16cb7bc5a208361087c0b400af5b
Noel Vock <noel1uk@gmail.com>
2019-07-21 15:48:53 +0100

    add scope, as default singletone

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -12,6 +12,11 @@ public class Application {
 //        CustomerService service = new CustomerServiceImpl();
 
         CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
+        System.out.println(service);
+
+        CustomerService service2 = applicationContext.getBean("customerService", CustomerService.class);
+        System.out.println(service2);
+
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
