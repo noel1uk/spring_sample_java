d28ced72954761abd668a2df8614467ba8f31e7b
Noel Vock <noel1uk@gmail.com>
2019-07-21 14:27:05 +0100

    setter injection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,17 @@
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        CustomerService service = new CustomerServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
+//        CustomerService service = new CustomerServiceImpl();
+
+        CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,13 +2,17 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository;
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public List<Customer> findAll() {
         return customerRepository.findAll();
