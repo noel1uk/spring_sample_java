c2c31be306093728d671b0df19493ff263e29534
Noel Vock <noel1uk@gmail.com>
2019-07-21 15:35:56 +0100

    autowire without declared beans in AppConfig

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -10,13 +10,13 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
-    @Bean(name = "customerService")
-    public CustomerService getCustomerService() {
-//        CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
-        CustomerServiceImpl service = new CustomerServiceImpl();
-//        service.setCustomerRepository(getCustomerRepository());
-        return service;
-    }
+//    @Bean(name = "customerService")
+//    public CustomerService getCustomerService() {
+////        CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
+//        CustomerServiceImpl service = new CustomerServiceImpl();
+////        service.setCustomerRepository(getCustomerRepository());
+//        return service;
+//    }
 
 //    @Bean(name = "customerRepository")
 //    public CustomerRepository getCustomerRepository() {
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,10 +2,13 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
